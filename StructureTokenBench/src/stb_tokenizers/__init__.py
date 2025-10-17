07ecee668988a0452b4c6d71d5ce4f7e03b28eb4
Farzane Esmaili <61351088+farz1995@users.noreply.github.com>
2025-10-17 12:58:18 -0500

    Delete StructureTokenBench directory

--- StructureTokenBench/src/stb_tokenizers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .wrapped_myrep import WrappedMyRepTokenizer, MissingRepresentation
-from .wrapped_myrep_rh import WrappedMyRepRemoteHomologyTokenizer
-from .wrapped_myrep_shake import WrappedMyRepShakeTokenizer
-
-# export any other local tokenizers here
--- StructureTokenBench/src/tokenizers/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.baselines.wrapped_myrep import WrappedMyRepTokenizer
-from .wrapped_myrep import WrappedMyRepTokenizer
-
-__all__ = ["WrappedMyRepTokenizer"]  
