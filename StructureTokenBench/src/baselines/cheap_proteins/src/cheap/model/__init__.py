6837c69571ca7d26f4803735dff410f6b5690e90
Farzane Esmaili <61351088+farz1995@users.noreply.github.com>
2025-10-17 12:34:04 -0500

    Delete StructureTokenBench/src directory

--- StructureTokenBench/src/baselines/cheap_proteins/src/cheap/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._modules import (
-    HourglassDecoder,
-    HourglassEncoder,
-    VectorQuantizer,
-    FiniteScalarQuantizer,
-)
-from ._hourglass import HourglassProteinCompressionTransformer
